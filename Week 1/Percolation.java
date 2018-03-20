27e4d28fcb548e0c62d02b8d399e75064564ac40
Thomas Keating <twkeating1@gmail.com>
2018-03-20 19:03:21 -0400

    initial commit

---
+++ Week 1/Percolation.java
@@ -0,0 +1,32 @@
+public class Percolation {
+    
+    public Percolation(int n) {
+        int[][] grid = new int[n][n];
+    }
+    
+    public void open (int row, int col) {
+        
+    }
+    
+    public boolean isOpen(int row, int col) {
+        return false;
+    }
+    
+    public boolean isFull(int row, int col) {
+        return false;
+    }
+    
+    public int numberofOpenSites() {
+        return 0;
+    }
+    
+    public boolean percolates() {
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        Percolation test = new Percolation(20);
+        //System.out.println(grid[0][0]);
+    }
+    
+}
