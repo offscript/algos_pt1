d79fbe45335897066236ec79d15cb802ad341bb1
Thomas Keating <twkeating1@gmail.com>
2018-03-22 19:15:36 -0400

    Initialization, Open, created

--- Week 1/Percolation.java
+++ Week 1/Percolation.java
@@ -1,15 +1,27 @@
 public class Percolation {
     
+    public int[][] grid;
+    public int[] component;
+    
     public Percolation(int n) {
-        int[][] grid = new int[n][n];
+        grid = new int[n][n];
+        //int count = 1;
+        //for (int i = 1; i < (n + 1); i++) {
+          //  for (int j = 1; j < (n + 1); j++) {
+            //    grid[i - 1][j - 1] = count;
+              //  count++;
+               // System.out.print(grid[i - 1][j - 1]);
+           // }
+      //  }
     }
     
-    public void open (int row, int col) {
-        
+    public void open(int row, int col) {
+        grid[row - 1][col - 1] = ((row - 1) * 20) + col;
+        System.out.println(grid[row - 1][col - 1]);
     }
     
     public boolean isOpen(int row, int col) {
-        return false;
+        return true ? grid[row][col] == 1 : grid[row][col] == 0;
     }
     
     public boolean isFull(int row, int col) {
@@ -26,7 +38,10 @@ public class Percolation {
     
     public static void main(String[] args) {
         Percolation test = new Percolation(20);
-        //System.out.println(grid[0][0]);
+        test.open(1,17);
+        test.open(7,13);
+        test.open(9,12);
+        test.open(20,19);
     }
     
 }
