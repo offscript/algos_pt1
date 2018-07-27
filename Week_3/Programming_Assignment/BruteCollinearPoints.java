913d76526a5d2b2e3e2608b1859ad045237b6b3e
Thomas Keating <twkeating1@gmail.com>
2018-07-27 07:53:58 -0400

    Bah God That's Thomas Keating's Music

--- Week_3/Programming_Assignment/BruteCollinearPoints.java
+++ Week_3/Programming_Assignment/BruteCollinearPoints.java
@@ -1,10 +1,18 @@
 public class BruteCollinearPoints {
    public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
    {
-       for (Point i : points) {
-         if (i.hasNext()) System.out.println("!");
+       for (int i = 0; i < points.length; i++) {
+           for (int j = 1; j < points.length; j++) {
+               for (int k = 2; k < points.length; j++) {
+                   //I'd use slope order here but idk how
+                   if points[i].slopeTo(points[j]) == points[i].slopeTo(points[k])
+                       for (int l == 3; l < points.length; l++) {
+                       if points[i].slopeTo(points[l]) == points[i].slopeTo(points[k])
+                           LineSegment segment = new LineSegment(point[i],point[l]);
+                   }
+               }
+           }
        }
-   
    }
    public int numberOfSegments()        // the number of line segments
    {}
